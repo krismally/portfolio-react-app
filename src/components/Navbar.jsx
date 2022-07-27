a46110e14bf67544f435d647fd22855997c23475
Kris Mally <kristophermally@gmail.com>
2022-07-26 18:07:19 -0600

    Add smooth scroll and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
+import Resume from '../assets/01-Kris-Mally.pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -17,11 +19,21 @@ const Navbar = () => {
         {/* menu */}
         
         <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+                <Link to='home' smooth={true} duration={500}>Home</Link>
+            </li>
+            <li>
+                <Link to='about' smooth={true} duration={500}>About</Link>
+            </li>
+            <li>
+                <Link to='skills' smooth={true} duration={500}>Skills</Link>
+            </li>
+            <li>
+                <Link to='projects' smooth={true} duration={500}>Projects</Link>
+            </li>
+            <li>
+                <Link to='contact' smooth={true} duration={500}>Contact</Link>
+            </li>
         </ul>
 
         {/* hamburger */}
@@ -30,33 +42,43 @@ const Navbar = () => {
         </div>
         {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Projects</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='skills' smooth={true} duration={500}>Skills</Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='projects' smooth={true} duration={500}>Projects</Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link>
+            </li>
         </ul>
 
         {/* social icons */}
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a className='w-full text-gray-300 flex justify-between items-center' href="/">
+                    <a className='w-full text-gray-300 flex justify-between items-center' href="https://linkedin.com/in/kris-mally" target='_blank'>
                         LinkedIn <FaLinkedin size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                    <a className='w-full text-gray-300 flex justify-between items-center' href="/">
+                    <a className='w-full text-gray-300 flex justify-between items-center' href="https://github.com/krismally" target='_blank'>
                         GitHub <FaGithub size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#217763]'>
-                    <a className='w-full text-gray-300 flex justify-between items-center' href="/">
+                    <a className='w-full text-gray-300 flex justify-between items-center' href="mailto:kristophermally@gmail.com">
                         Email <HiOutlineMail size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='w-full text-gray-300 flex justify-between items-center' href="/">
+                    <a className='w-full text-gray-300 flex justify-between items-center' href={Resume} target='_blank'>
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import Marvel from '../assets/marvel-character-guide.png'
 
 const Projects = () => {
   return (
-    <div name='work' className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div name='projects' className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
         <div className="max-w-[1000px] mx-auto p-4 px-10 flex flex-col justify-center w-full h-full">
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-[#217763] text-gray-300">Projects</p>
