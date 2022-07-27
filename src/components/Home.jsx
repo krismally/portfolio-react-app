dbe9b170a924eb5b7f4e1b6f70215e4e0dadc316
Kris Mally <kristophermally@gmail.com>
2022-07-26 19:10:33 -0600

    Add hover effect for navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,12 +13,12 @@ const Home = () => {
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I specialize in crafting beautiful digital experiences. Currently, I'm focused on building well-designed, responsive full-stack web applications.</p>
             <div>
                 <Link to='projects' smooth={true} duration={500}>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#217763]'>
-                    View Work 
-                    <span className='group-hover:rotate-90 duration-300'>
-                        <HiArrowNarrowRight className='ml-2' />
-                    </span>
-                </button>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#217763]'>
+                        View Work 
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-2' />
+                        </span>
+                    </button>
                 </Link>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,19 +19,19 @@ const Navbar = () => {
         {/* menu */}
         
         <ul className='hidden md:flex'>
-            <li>
+            <li className='hover:text-[#217763]'>
                 <Link to='home' smooth={true} duration={500}>Home</Link>
             </li>
-            <li>
+            <li className='hover:text-[#217763]'>
                 <Link to='about' smooth={true} duration={500}>About</Link>
             </li>
-            <li>
+            <li className='hover:text-[#217763]'>
                 <Link to='skills' smooth={true} duration={500}>Skills</Link>
             </li>
-            <li>
+            <li className='hover:text-[#217763]'>
                 <Link to='projects' smooth={true} duration={500}>Projects</Link>
             </li>
-            <li>
+            <li className='hover:text-[#217763]'>
                 <Link to='contact' smooth={true} duration={500}>Contact</Link>
             </li>
         </ul>
@@ -42,19 +42,19 @@ const Navbar = () => {
         </div>
         {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>
+            <li className='py-6 text-4xl hover:text-[#217763]'>
                 <Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link>
             </li>
-            <li className='py-6 text-4xl'>
+            <li className='py-6 text-4xl hover:text-[#217763]'>
                 <Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link>
             </li>
-            <li className='py-6 text-4xl'>
+            <li className='py-6 text-4xl hover:text-[#217763]'>
                 <Link onClick={handleClick} to='skills' smooth={true} duration={500}>Skills</Link>
             </li>
-            <li className='py-6 text-4xl'>
+            <li className='py-6 text-4xl hover:text-[#217763]'>
                 <Link onClick={handleClick} to='projects' smooth={true} duration={500}>Projects</Link>
             </li>
-            <li className='py-6 text-4xl'>
+            <li className='py-6 text-4xl hover:text-[#217763]'>
                 <Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link>
             </li>
         </ul>
