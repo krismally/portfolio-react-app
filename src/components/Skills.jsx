9e6ebcc7f8c91016f5d644062067c78c79d5a782
Kris Mally <kristophermally@gmail.com>
2022-07-26 19:01:07 -0600

    Correct mobile layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 
 const Skills = () => {
   return (
-    <div name='skills' className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div name='skills' className="w-full md:h-screen bg-[#0a192f] text-gray-300">
     {/* Container */}
     <div className="max-w-[1000px] mx-auto p-4 px-10 flex flex-col justify-center w-full h-full">
         <div>
